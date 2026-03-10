52742f76844320cb868d75cf5fcc81072989ce09
Carlos Brida <carloscauedb@gmail.com>
2026-03-10 12:37:28 -0300

    Update urls.py

--- backend/tickets/presentation/urls.py
+++ backend/tickets/presentation/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from tickets.presentation.views import HomeView
+
+from tickets.presentation import views
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
+    path("", views.ticket_list, name="ticket_list"),
+    path("tickets/novo/", views.ticket_create, name="ticket_create"),
+    path("tickets/<uuid:ticket_id>/editar/", views.ticket_edit, name="ticket_edit"),
+    path("tickets/<uuid:ticket_id>/excluir/", views.ticket_delete, name="ticket_delete"),
+    path("tickets/<uuid:ticket_id>/detalhe/", views.ticket_detail, name="ticket_detail"),
 ]
